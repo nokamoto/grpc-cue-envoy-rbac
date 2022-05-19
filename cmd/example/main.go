323049a5ee673dd91daa4f3b9d424a49a7eed9a0
Naofumi Okamoto <nokamoto.engr@gmail.com>
2022-05-19 15:24:36 +0000

    Run example server with skaffold

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/nokamoto/grpc-cue-envoy-rbac/pkg/api/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type example struct {
+	api.UnimplementedExampleServiceServer
+}
+
+func main() {
+	port := 9000
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	api.RegisterExampleServiceServer(s, &example{})
+	reflection.Register(s)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
