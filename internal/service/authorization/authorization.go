c1ccd0d35d7d708fb16bd08514f53347fc56858f
Naofumi Okamoto <nokamoto.engr@gmail.com>
2022-05-25 08:36:48 +0000

    Implement always deny authorization service

--- internal/service/authorization/authorization.go
+++ internal/service/authorization/authorization.go
@@ -6,6 +6,7 @@ import (
 
 	v3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
 	"github.com/nokamoto/grpc-cue-envoy-rbac/internal/rbac/plugin"
+	"github.com/nokamoto/grpc-cue-envoy-rbac/pkg/api"
 	"google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc/codes"
 )
@@ -33,11 +34,27 @@ func newResponse(code codes.Code, message string) *v3.CheckResponse {
 	}
 }
 
-func (*Authorization) Check(ctx context.Context, req *v3.CheckRequest) (*v3.CheckResponse, error) {
+func (a *Authorization) match(path string) *api.ExternalAuthorization_Rule {
+	for _, rule := range a.cfg.GetRules() {
+		if rule.GetPath() == path {
+			return rule
+		}
+	}
+	return nil
+}
+
+func (a *Authorization) authorize(ctx context.Context, req *v3.CheckRequest, rule *api.ExternalAuthorization_Rule) *v3.CheckResponse {
+	if rule == nil {
+		return newResponse(codes.OK, "")
+	}
+	return newResponse(codes.Unimplemented, "unimplemented")
+}
+
+func (a *Authorization) Check(ctx context.Context, req *v3.CheckRequest) (*v3.CheckResponse, error) {
 	path := req.GetAttributes().GetRequest().GetHttp().GetPath()
 	log.Printf("Check: %v", path)
 	if path == reflectionPath {
 		return newResponse(codes.OK, ""), nil
 	}
-	return newResponse(codes.PermissionDenied, "permission denied"), nil
+	return a.authorize(ctx, req, a.match(path)), nil
 }
--- internal/service/authorization/authorization_test.go
+++ internal/service/authorization/authorization_test.go
@@ -6,6 +6,7 @@ import (
 
 	v3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
 	"github.com/google/go-cmp/cmp"
+	"github.com/nokamoto/grpc-cue-envoy-rbac/pkg/api"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/testing/protocmp"
@@ -25,10 +26,11 @@ func newRequest(path string) *v3.CheckRequest {
 
 func TestAuthorization_Check(t *testing.T) {
 	tests := []struct {
-		name string
-		req  *v3.CheckRequest
-		want *v3.CheckResponse
-		code codes.Code
+		name  string
+		rules []*api.ExternalAuthorization_Rule
+		req   *v3.CheckRequest
+		want  *v3.CheckResponse
+		code  codes.Code
 	}{
 		{
 			name: "allow reflection",
@@ -36,14 +38,44 @@ func TestAuthorization_Check(t *testing.T) {
 			want: newResponse(codes.OK, ""),
 		},
 		{
-			name: "permission dinied",
+			name: "allow unknown path",
 			req:  newRequest("/unknown"),
-			want: newResponse(codes.PermissionDenied, "permission denied"),
+			want: newResponse(codes.OK, ""),
+		},
+		{
+			name: "todo: allow if authorization passed",
+			rules: []*api.ExternalAuthorization_Rule{
+				{
+					Path: "/ok",
+					Authorization: &api.Authorization{
+						Permission: "foo",
+					},
+				},
+			},
+			req:  newRequest("/ok"),
+			want: newResponse(codes.Unimplemented, "unimplemented"),
+		},
+		{
+			name: "todo: deny if authorization failed",
+			rules: []*api.ExternalAuthorization_Rule{
+				{
+					Path: "/ok",
+					Authorization: &api.Authorization{
+						Permission: "foo",
+					},
+				},
+			},
+			req:  newRequest("/ok"),
+			want: newResponse(codes.Unimplemented, "unimplemented"),
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			a := &Authorization{}
+			a := &Authorization{
+				cfg: &api.ExternalAuthorization{
+					Rules: test.rules,
+				},
+			}
 			got, err := a.Check(context.TODO(), test.req)
 			if status.Code(err) != test.code {
 				t.Errorf("Check() error = %v, want %v", err, test.code)
